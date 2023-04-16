c643d553dffca78cd16a323875ac3d856d1ca6cf
Gene014 <calmaeugene06@gmail.com>
2023-04-16 15:39:53 +0800

    checking and completing the variables from a-u, making sure it's running

--- decryptorcode.py
+++ decryptorcode.py
@@ -7,8 +7,21 @@ input_str = input("Hi, Good day! Please insert your text here. ")
 output_str = ""
 # check and change every character to their corresponding variable
 for i in range (len(input_str)):
-     #   if *, change to a
+#   if *, change to a
     if input_str[i] == "*":
         output_str += "a"
+#   if &, change to e
+    elif input_str[i] == "&":
+        output_str += "e"
+#   if #, change to i
+    elif input_str[i] == "#":
+        output_str += "i"
+#   if +, change to o
+    elif input_str[i] == "+":
+        output_str += "o"  
+#   if !, change to u
+    elif input_str[i] == "!":
+        output_str += "u"
     else: output_str += input_str[i]
+# print your output
 print(output_str)
