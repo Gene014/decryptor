db24bcbcb40cad5e3b13abf18a566e0b0e4e6106
Gene014 <calmaeugene06@gmail.com>
2023-04-16 15:20:43 +0800

    input string running

---
+++ decryptorcode.py
@@ -0,0 +1,6 @@
+#Calma, Eugene Marie S.'
+#BSCPE_1-4
+#OOP Laboratory Exercise Number 2 Problem 2 (Decryption)
+
+# input or insert your text here
+input_str = input("Hi, Good day! Please insert your text here. ")
