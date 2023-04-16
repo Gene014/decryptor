55a7bbf7b08973d215ed72c95697b2a7161dd32b
Gene014 <calmaeugene06@gmail.com>
2023-04-16 15:29:02 +0800

    check and change every character to their corresponding variable and insert output string running

--- decryptorcode.py
+++ decryptorcode.py
@@ -4,3 +4,11 @@
 
 # input or insert your text here
 input_str = input("Hi, Good day! Please insert your text here. ")
+output_str = ""
+# check and change every character to their corresponding variable
+for i in range (len(input_str)):
+     #   if *, change to a
+    if input_str[i] == "*":
+        output_str += "a"
+    else: output_str += input_str[i]
+print(output_str)
